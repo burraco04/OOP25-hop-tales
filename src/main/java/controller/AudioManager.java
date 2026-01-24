da63a6a7f9bdf9ebbda9dcfbad0df9eaa8876a77
Enrico Casadei <enrico.casadei18@studio.unibo.it>
2026-01-24 12:33:30 +0100

    Aggiunta javadoc

--- src/main/java/controller/AudioManager.java
+++ src/main/java/controller/AudioManager.java
@@ -7,9 +7,18 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
+/**
+ * Class that handles audio files.
+ */
 public class AudioManager {
     private static final Map<String, Clip> sounds = new HashMap<>();
 
+    /**
+     * Register a given file into a map of sounds usable in the game.
+     * 
+     * @param name name used to address the file in the map.
+     * @param path path of the file.
+     */
     public static void load(String name, String path) {
         try {
             AudioInputStream ais = AudioSystem.getAudioInputStream(
@@ -23,6 +32,11 @@ public class AudioManager {
         }
     }
 
+    /**
+     * Play the selected song or audio.
+     * 
+     * @param name name of the audio in the map.
+     */
     public static void play(String name) {
         Clip clip = sounds.get(name);
         if (clip == null) return;

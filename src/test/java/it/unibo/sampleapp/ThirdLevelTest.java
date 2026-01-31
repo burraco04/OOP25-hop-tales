7b64588fc9a51972899ed11aab36e0ecaea13043
Enrico Casadei <enrico.casadei18@studio.unibo.it>
2026-01-31 16:32:58 +0100

    Aggiunto test per il terzo livello

---
+++ src/test/java/it/unibo/sampleapp/ThirdLevelTest.java
@@ -0,0 +1,19 @@
+package it.unibo.sampleapp;
+
+import javax.swing.JFrame;
+
+import game.FireboyWatergirlLevel;
+
+public class ThirdLevelTest {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Third Level");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(new FireboyWatergirlLevel());
+
+        frame.setSize(800, 600); // O una dimensione adatta
+        frame.setLocationRelativeTo(null); // centra la finestra
+        frame.setVisible(true);
+    }
+}
+
